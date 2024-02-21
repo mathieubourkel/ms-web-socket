46336748fb918a02c2d0a315aacdab5c6bf1128e
mathieubourkel <mathieu.bourkel@gmail.com>
2024-02-21 20:21:48 +0100

    add ssl web socket

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as fs from "fs"
+
+const httpsOptions = {
+  key: fs.readFileSync(process.env.BACK_URL_KEY, 'utf-8'),
+  cert: fs.readFileSync(process.env.BACK_URL_CERT, 'utf-8'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  let app;
+  if (process.env.NODE_ENV == 'production'){
+    app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions});
+  } else {
+    app = await NestFactory.create<NestExpressApplication>(AppModule);
+  }
   app.listen(process.env.MS_WEB_SOCKET_PORT, () => {
     console.log(`[MS-WEB-SOCKET] with NATS on ${process.env.MS_WEB_SOCKET_PORT}`)
   });
